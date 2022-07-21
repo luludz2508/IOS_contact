1b3d8a49ade10c3aa72bcad86f98761ad918d7d3
s3757937 <s3757937@rmit.edu.vn>
2022-07-21 13:15:20 +0700

    Initial Commit

---
+++ IOS_contact/IOS_contactApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_contactApp.swift
+//  IOS_contact
+//
+//  Created by Luan, Nguyen Thanh on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct IOS_contactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
